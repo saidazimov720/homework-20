7c14895c5d56ab87c2d11799f5061a977af2ea31
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-15 19:49:30 +0500

    function continue

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ let isGameActive = false;
 function startGame() {
     if (isGameActive) return;
     isGameActive = true;
-    
+    let timeLeft = 5;
+    document.getElementById('timer').textContent = `Time left ${timeLeft} s`;
+    countdown = setInterval(() => {
+        timeLeft--;
+        document.getElementById('timer').textContent = `Time left ${timeLeft}`;
+        
+    })
 }
