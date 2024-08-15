48240089b6ad7098bf93cf353294ddfbc1ee8204
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-15 19:52:44 +0500

    end the startGame function

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function startGame() {
     countdown = setInterval(() => {
         timeLeft--;
         document.getElementById('timer').textContent = `Time left ${timeLeft}`;
-        
-    })
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            document.getElementById('timer').textContent = 'Time is up';
+            isGameActive = false;
+        }
+    }, 1000);
 }
